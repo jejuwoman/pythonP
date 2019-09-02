5164da5711efb1469a8e12f0f0406f567ed0e676
jejuwoman <jejuwoman@gmail.com>
2019-09-02 12:52:30 +0900

    20190902 2번째 커밋

--- basic/test1.py
+++ basic/test1.py
@@ -1,1 +1,24 @@
 print("hi")
+a=3
+b=4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+s = 'pyt\nhon'
+s1 = "python"
+s2 = "python's"
+s3 = '"python"'
+s4 = """python"""
+s5 = "py"
+s6 = '''p
+        y
+        '''
+print(s)
+
+head = "10"
+tail = "is fun"
+print(head+tail)
+print(type(head))
+print(type(tail))
